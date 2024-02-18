b15f5d32ad61afc03b8ea90a0ed0ed457a49a610
Maxim Maximovich <maxim90m@gmail.com>
2024-02-18 10:12:23 +0200

    create basic structure with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import Home from "./pages/home";
+import Resume from "./pages/resume";
+
+const Preloader = () => <div>Loading...</div>;
+const Fallback = () => <div>404</div>;
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={
+            <Suspense fallback={<Preloader />}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path=":username"
+          element={
+            <Suspense fallback={<Preloader />}>
+              <Resume />
+            </Suspense>
+          }
+        />
+        <Route path="*" element={<Fallback />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
